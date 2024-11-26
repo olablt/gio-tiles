c5c2b7125b30868bbcd3ca612d84db131d61cfbc
Melitonas <melitonas@gmail.com>
2024-11-26 14:29:09 +0200

    refactor: Switch back to local tile provider for map rendering

--- apps/hello/main.go
+++ apps/hello/main.go
@@ -29,10 +29,10 @@ type MapView struct {
 
 func NewMapView() *MapView {
 	return &MapView{
-		// tileManager: maps.NewTileManager(maps.NewLocalTileProvider()), // Use local provider
-		tileManager: maps.NewTileManager(maps.NewOSMTileProvider()), // Use OSM provider
-		center:      maps.LatLng{Lat: 51.507222, Lng: -0.1275},      // London
-		zoom:        4,
+		tileManager: maps.NewTileManager(maps.NewLocalTileProvider()), // Use local provider
+		// tileManager: maps.NewTileManager(maps.NewOSMTileProvider()), // Use OSM provider
+		center: maps.LatLng{Lat: 51.507222, Lng: -0.1275}, // London
+		zoom:   4,
 		list: &widget.List{
 			List: layout.List{
 				Axis: layout.Vertical,
